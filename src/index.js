2d2b27aad6184c6fa81d7e8ba67ccc0bcb29ffe2
0Shree005 <shreepingale12@gmail.com>
2024-03-14 22:56:08 +0530

    Integrated MongoDB and configured the /add command

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 require('dotenv').config();
 
-const { Client, IntentsBitField, moveElementInArray } = require('discord.js')
+const { Client, IntentsBitField } = require('discord.js')
+const mongoose = require('mongoose');
 
-// const arr= [];
-const watchLists = new Map()
 
+// Connect to Database 
+mongoose.connect(process.env.DATABASE);
+
+const movieSchema = new mongoose.Schema({
+    movie_name: String,
+    preference_value: Number
+});
+
+// Creating a model for movies
+// const UserChoice = new DiscordMovies({ 
+//     movie_name: userMovieName,
+//     preference_value: userMoviePref
+// });
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+// Initialising Discord Permissions
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -14,35 +30,40 @@ const client = new Client({
     ]
 })
 
-
+// Conformation for Bot's status
 client.on("ready", (c) => {
     console.log(`${c.user.tag} is online`)
 })
 
-client.on('interactionCreate', (interaction) => {
-
-
+// Functionality of Custom Slash commands
+client.on('interactionCreate', async (interaction) => {
     if (!interaction.isChatInputCommand()) 
         return;
 
     const userId = interaction.user.id
 
-
+    
+    // "/add"
     if (interaction.commandName === 'add'){
         const userMovieName = interaction.options.getString('movie_name');
         const userMoviePref = interaction.options.getNumber('preference_value');
-        console.log(interaction.options)
-        const userWatchList = watchLists.get(userId) || [] 
-        userWatchList.push({ name: userMovieName, preference: userMoviePref })
-        // console.log(userMovieName)
-        watchLists.set(userId, userWatchList)
+
+        await Movie.create({ movie_name: userMovieName, preference_value: userMoviePref});
+        // const userWatchList = watchLists.get(userId) || [] 
+        // userWatchList.push({ name: userMovieName, preference: userMoviePref })
+
+        // console.log(userWatchList)
+        // watchLists.set(userId, userWatchList)
         interaction.reply(`<@${userId}> Your movie **${userMovieName}** was added to your watchlist`);
     }
+
+    // "/watchlist"
     if (interaction.commandName === 'watchlist'){
-        const userWatchList = watchLists.get(userId) || [];
+
+        const userMovies = await Movie.find({})
 
         let styledWatchlist = "Your current watchlist:\n"
-        userWatchList.forEach((movie, index) => {
+        userMovies.forEach((movie, index) => {
             styledWatchlist += `${index + 1}. **${movie.name}** (Preference: ${movie.preference})\n`            
         });
         interaction.reply(`<@${userId}> Your current watchlist ${styledWatchlist}`);
